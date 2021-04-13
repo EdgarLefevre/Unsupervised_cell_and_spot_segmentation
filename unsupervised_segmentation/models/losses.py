:bug: starting to fix weights and loss calculation

unsupervised_segmentation/models/losses.py
@@ -9,7 +9,7 @@ def edge_weights(flatten_image, rows, cols, std_intensity=3, std_position=1.0):
     flatten_image : 1 dim tf array of the row flattened image
     ( intensity is the average of the three channels)
     std_intensity : standard deviation for intensity
-    std_position : standard devistion for position
+    std_position : standard deviation for position
     rows : rows of the original image (unflattened image)
     cols : cols of the original image (unflattened image)
     Output :
@@ -78,7 +78,7 @@ def denominator(k_class_prob, weights):
     )
 
 
-def soft_n_cut_loss(flatten_image, prob, k, rows, cols):
+def soft_n_cut_loss_(flatten_image, prob, k, rows, cols):
     """
     Inputs:
     prob : (rows*cols*k) tensor
@@ -95,6 +95,17 @@ def soft_n_cut_loss(flatten_image, prob, k, rows, cols):
     weights = edge_weights(flatten_image, rows, cols)
     for t in range(k):
         loss = loss - (numerator(prob, weights) / denominator(prob, weights))
+    return loss
+
+
+def soft_n_cut_loss(inputs, segmentations):
+    # We don't do n_cut_loss batch wise -- split it up and do it instance wise
+    loss = 0
+    for i in range(inputs.shape[0]):
+        flatten_image = tf.reduce_mean(inputs[i], axis=-1)
+        flatten_image = tf.reshape(flatten_image, (flatten_image.shape[0] ** 2))
+        loss += soft_n_cut_loss_(flatten_image, segmentations[i], 1, 128, 128)
+    loss = loss / inputs.shape[0]
     return loss
 
 

unsupervised_segmentation/train_singlegpu.py
@@ -117,11 +117,129 @@ def run_epoch(
                 train,
             )
     if not train:
-        x = tf.convert_to_tensor(
-            x.numpy()[0].reshape(-1, 128, 128, 1), dtype=tf.float32
-        )
+        x = tf.convert_to_tensor(x[0].reshape(-1, 128, 128, 1), dtype=tf.float32)
         utrain.visualize(gen, model_wnet, x, epoch + 1, opt)
     return loss
+
+
+def loss2(gen, wnet, image):
+    loss_ncut = losses.soft_n_cut_loss
+    loss_recons = keras.metrics.mse
+    pred = gen(image)
+    gen_loss = tf.abs(loss_ncut(image, pred))
+    output = wnet(image)
+    wnet_loss = tf.cast(
+        loss_recons(keras.backend.flatten(image), keras.backend.flatten(output)),
+        dtype=tf.double,
+    )
+    return gen_loss, wnet_loss
+
+
+def _grad2(gen, wnet, image):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as wnet_tape:
+        gen_loss, wnet_loss = loss2(gen, wnet, image)
+    return (
+        gen_loss,
+        wnet_loss,
+        gen_tape.gradient(gen_loss, gen.trainable_variables),
+        wnet_tape.gradient(wnet_loss, wnet.trainable_variables),
+    )
+
+
+def _step2(
+    gen,
+    model_wnet,
+    x,
+    optimizer_gen,
+    optimizer_wnet,
+    train=True,
+):
+    epoch_loss_avg = tf.keras.metrics.Mean()
+    gen_loss, wnet_loss, gen_grads, wnet_grads = _grad2(
+        gen,
+        model_wnet,
+        x,
+    )
+    if train:
+        optimizer_gen.apply_gradients(zip(gen_grads, gen.trainable_variables))
+        optimizer_wnet.apply_gradients(zip(wnet_grads, model_wnet.trainable_variables))
+    return epoch_loss_avg(gen_loss + wnet_loss)
+
+
+def run_epoch2(
+    dataset,
+    gen,
+    model_wnet,
+    o_gen,
+    o_wnet,
+    epoch,
+    train=True,
+):
+    loss = 0
+    with progressbar.ProgressBar(max_value=len(dataset), widgets=widgets) as bar:
+        for i, x in enumerate(dataset):
+            bar.update(i)
+            loss += _step2(
+                gen,
+                model_wnet,
+                x,
+                o_gen,
+                o_wnet,
+                train,
+            )
+    if not train:
+        x = tf.convert_to_tensor(x[0].reshape(-1, 128, 128, 1), dtype=tf.float32)
+        utrain.visualize(gen, model_wnet, x, epoch + 1, opt)
+    return loss
+
+
+def train2():
+    img_path_list = utils.list_files_path(opt.img_path)
+    # not good if we need to do metrics
+    img_train, img_test = sk.train_test_split(
+        img_path_list, test_size=0.2, random_state=42
+    )
+
+    dataset_train = data.Dataset2(opt.batch_size, opt.size, img_train)
+    dataset_test = data.Dataset2(opt.batch_size, opt.size, img_test)
+    gen, model_wnet = wnet.wnet(input_shape=(opt.size, opt.size, 1))
+    optimizer_gen = tf.keras.optimizers.Adam(opt.lr)
+    optimizer_wnet = tf.keras.optimizers.Adam(opt.lr)
+    train_loss_list = []
+    test_loss_list = []
+    utils.print_gre("Training....")
+    for epoch in range(opt.n_epochs):
+        utils.print_gre("Epoch {}/{}:".format(epoch + 1, opt.n_epochs))
+        utils.print_gre("Training data:")
+        train_loss = run_epoch2(
+            dataset_train,
+            gen,
+            model_wnet,
+            optimizer_gen,
+            optimizer_wnet,
+            epoch,
+        )
+        utils.print_gre("Testing data:")
+        test_loss = run_epoch2(
+            dataset_test,
+            gen,
+            model_wnet,
+            optimizer_gen,
+            optimizer_wnet,
+            epoch,
+            False,
+        )
+        utils.print_gre(
+            "Epoch {:03d}/{:03d}: Loss: {:.3f} Test_Loss: {:.3f}".format(
+                epoch + 1, opt.n_epochs, train_loss, test_loss
+            )
+        )
+        train_loss_list.append(train_loss)
+        test_loss_list.append(test_loss)
+        utrain.is_nan(train_loss, test_loss, epoch)
+        utrain.reduce_lr(epoch, 10, optimizer_gen, optimizer_wnet)
+        save(gen, model_wnet, test_loss, PATH_SAVE_gen, PATH_SAVE_wnet)
+        utrain.plot(train_loss_list, test_loss_list)
 
 
 def train():

unsupervised_segmentation/utils/data.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
 
-import random
-
 import cupy as cp
 import numpy as np
 import progressbar
-import skimage.io as io
-import sklearn.model_selection as sk
-import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.preprocessing.image import load_img
-
-import unsupervised_segmentation.utils.utils as utils
 
 # widget list for the progress bar
 widgets = [
@@ -24,47 +17,6 @@ widgets = [
     progressbar.ETA(),
     ") ",
 ]
-
-
-def get_dataset(path_imgs, args):
-    utils.print_gre("Creating dataset...")
-    dataset = []
-    file_list = utils.list_files(path_imgs)  # add [:10] to reduce dataset size
-    for file in file_list:
-        img = io.imread(path_imgs + file, plugin="tifffile")
-        img = np.array(img).reshape(-1, args.size, args.size, 1).astype("float32")
-        # rm img/255
-        # -> already done ?? for 128.... strange behavior
-        dataset.append(img)
-    n = range(np.shape(dataset)[0])
-    n_sample = random.sample(list(n), len(n))
-    dataset = np.array(dataset)
-    dataset = dataset.reshape(-1, args.size, args.size, 1)[n_sample]
-    weights = get_weights(dataset)
-    dataset_train, dataset_test, weights_train, weights_test = sk.train_test_split(
-        dataset, weights, test_size=0.2, random_state=42
-    )
-    len_train = len(dataset_train)
-    len_test = len(dataset_test)
-    ds_train = (
-        tf.data.Dataset.from_tensor_slices((dataset_train, weights_train))
-        .shuffle(len_train)
-        .batch(args.batch_size)
-    )
-    ds_test = (
-        tf.data.Dataset.from_tensor_slices((dataset_test, weights_test))
-        .shuffle(len_test)
-        .batch(args.batch_size)
-    )
-    utils.print_gre("Dataset created !")
-    utils.print_red("Size of training set : {}".format(len_train))
-    utils.print_red("Size of testing set : {}".format(len_test))
-    return (
-        ds_train,
-        ds_test,
-        len_train,
-        len_test,
-    )
 
 
 def cal_weight(raw_data, shape, radius=5, sigmaI=10, sigmaX=4):
@@ -104,20 +56,17 @@ def cal_weight(raw_data, shape, radius=5, sigmaI=10, sigmaX=4):
 
 
 def get_weights(raw_data):
-    # utils.print_gre("Calculating weights...")
     weights = []
-    # raw_data = np.rollaxis(raw_data, 3, 1)
     shape = raw_data.shape
-    # with progressbar.ProgressBar(max_value=shape[0], widgets=widgets) as bar:
-    for batch_id in range(0, shape[0]):
-        # bar.update(batch_id)
-        batch = raw_data[batch_id : min(shape[0], batch_id + 1)]
-        tmp_weight = cal_weight(batch, batch.shape)
+    for img in raw_data:
+        img = cp.expand_dims(img, axis=0)
+        sigI = cp.sum((img - cp.mean(img)) ** 2) / (shape[1] * shape[2])
+        tmp_weight = cal_weight(img, img.shape, sigmaI=sigI)
         weight = cp.asnumpy(tmp_weight)
         weights.append(weight)
         del tmp_weight
+        del weight
     cp.get_default_memory_pool().free_all_blocks()
-    # utils.print_gre("Weights calculated.")
     return weights
 
 
@@ -160,3 +109,43 @@ class Dataset(keras.utils.Sequence):
             x[j] = np.expand_dims(img, 2)
         w = get_weights(x)
         return x, w
+
+
+class Dataset2(keras.utils.Sequence):
+    def __init__(
+        self,
+        batch_size,
+        img_size,
+        input_img_paths,
+    ):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+
+    def __len__(self):
+        return len(self.input_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """
+        Returns tuple (input, target) correspond to batch #idx.
+
+        For wnet -> should return only x+weight map (couple)
+        """
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        x = np.zeros(
+            (self.batch_size, self.img_size, self.img_size, 1), dtype="float32"
+        )
+        for j, path in enumerate(batch_input_img_paths):
+            img = (
+                np.array(
+                    load_img(
+                        path,
+                        color_mode="grayscale",
+                        target_size=(self.img_size, self.img_size),
+                    )
+                )
+                / 255
+            )
+            x[j] = np.expand_dims(img, 2)
+        return x